2b8a477b0984f0fd7996157bfd831fe91c0513eb
eshwarkole1641-esh <eshwarkole1641@gmail.com>
2026-02-10 19:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (47.51%) - LeetSync

---
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public boolean haveConflict(String[] event1, String[] event2) {
+     return !(event1[1].compareTo(event2[0])<0||event2[1].compareTo(event1[0])<0);
+
+    }
+}
