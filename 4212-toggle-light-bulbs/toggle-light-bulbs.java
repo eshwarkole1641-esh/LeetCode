59af56ac944cf2dad103dcb6f896cbb250d09fdc
eshwarkole1641-esh <eshwarkole1641@gmail.com>
2026-02-15 21:51:12 +0530

    Time: 2 ms (99.97%) | Memory: 46.8 MB (89.32%) - LeetSync

---
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
+       int arr[]=new int[101];
+       for(int i=0;i<bulbs.size();i++){
+        arr[bulbs.get(i)]++;
+       }
+       ArrayList<Integer>res=new ArrayList<>();
+       for(int i=0;i<arr.length;i++){
+        if(arr[i]%2==1){
+            res.add(i);
+        }
+       }
+       return res;
+
+
+    }
+}
