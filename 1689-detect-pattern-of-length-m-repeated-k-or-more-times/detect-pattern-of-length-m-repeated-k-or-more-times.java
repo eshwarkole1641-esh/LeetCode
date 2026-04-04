bb60338d0b93c2943c8372f7dc6dbe014b72221b
eshwarkole1641-esh <eshwarkole1641@gmail.com>
2026-04-04 15:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (81.66%) - LeetSync

--- 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.java
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.java
@@ -1,21 +1,34 @@
 class Solution {
     public boolean containsPattern(int[] arr, int m, int k) {
         int n=arr.length;
-         for(int i=0;i<=n-m*k;i++){
-            boolean valid=true;
-          for(int j=1;j<k;j++){
-            for(int x=0;x<m;x++){
-               if(arr[i+x]!=arr[i+(j*m)+x]){
-                  valid=false;
-                  break;
-               } 
+        int count=0;
+        for(int i=0;i<n-m;i++){
+            if(arr[i]==arr[i+m]){
+                count++;
             }
-             if(!valid){
-            break;
-         }
-          }
-         if(valid)return true;
+            else{
+                count=0;
+            }
+            if(count==(k-1)*m){
+                return true;
+            }
         }
-      return false;
+        return false;
+//          for(int i=0;i<=n-m*k;i++){
+//             boolean valid=true;
+//           for(int j=1;j<k;j++){
+//             for(int x=0;x<m;x++){
+//                if(arr[i+x]!=arr[i+(j*m)+x]){
+//                   valid=false;
+//                   break;
+//                } 
+//             }
+//              if(!valid){
+//             break;
+//          }
+//           }
+//          if(valid)return true;
+//         }
+//       return false;
     }
 }
