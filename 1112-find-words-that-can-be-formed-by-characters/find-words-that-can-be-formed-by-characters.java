8f6d6c6009cdb74a92b97b12fd51e329784dc513
eshwarkole1641-esh <eshwarkole1641@gmail.com>
2026-04-17 18:12:26 +0530

    Time: 6 ms (88.00%) | Memory: 47.8 MB (18.38%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,46 +1,48 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        // HashSet<Character>h=new HashSet<>();
-        // for(int i=0;i<chars.length();i++){
-        //     h.add(chars.charAt(i));
-        // }
-        // int count=0;
-        // for(int i=0;i<words.length;i++){
-        //     String s=words[i];
-        //     int c=0;
-        //     for(int j=0;j<s.length();j++){
-        //         if(h.contains(s.charAt(j))){
-        //              c++;
-        //         }
-        //         else{
+        //BRUTEFORCE
+        // int total=0;
+        // for(String word:words){
+        //     int fre[]=new int[26];
+        //     for(char c:chars.toCharArray()){
+        //         fre[c-'a']++;
+
+        //     }
+        //     boolean valid=true;
+        //     for(char c:word.toCharArray()){
+        //         fre[c-'a']--;
+        //         if(fre[c-'a']<0){
+        //             valid=false;
         //             break;
         //         }
+
         //     }
-        //    if(c==s.length()){
-        //     count+=c;
-        //    } 
+        //     if(valid){
+        //         total+=word.length();
+        //     }
         // }
-        // return count;
+
+        // OPTIMAL
         int total=0;
-        for(String word:words){
-            int fre[]=new int[26];
+           int base[]=new int[26];
             for(char c:chars.toCharArray()){
-                fre[c-'a']++;
-
+                base[c-'a']++;
             }
-            boolean valid=true;
-            for(char c:word.toCharArray()){
-                fre[c-'a']--;
-                if(fre[c-'a']<0){
-                    valid=false;
-                    break;
+            for(String word:words){
+                int temp[]=base.clone();
+                boolean valid=true;
+                for(char c:word.toCharArray()){
+                    temp[c-'a']--;
+                    if(temp[c-'a']<0){
+                        valid=false;
+                        break;
+                    }
                 }
-
-            }
-            if(valid){
+                 if(valid){
                 total+=word.length();
+                }
             }
-        }
+
         return total;
     }
 }
