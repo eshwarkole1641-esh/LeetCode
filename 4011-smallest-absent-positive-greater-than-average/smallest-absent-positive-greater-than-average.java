8cf8226ee547c7ccdc73f793fae94ecced6160e7
eshwarkole1641-esh <eshwarkole1641@gmail.com>
2026-04-19 12:08:26 +0530

    Time: 8 ms (31.59%) | Memory: 47 MB (41.25%) - LeetSync

--- 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.java
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.java
@@ -1,7 +1,7 @@
 
 class Solution {
     public int smallestAbsent(int[] nums) {
-       Arrays.sort(nums);
+    //    Arrays.sort(nums);
        int avg=0;
        for(int i=0;i<nums.length;i++){
         avg+=nums[i];
@@ -10,13 +10,35 @@ class Solution {
        avg=(avg/nums.length);
        else
        avg=0;
-    //    avg+=1;
-       boolean x=true;
-       for(int i=0;i<nums.length;i++){
-        if(avg+1==nums[i]){
-            avg+=1;
-        }
-       }
-       return avg+1;
+    // //    avg+=1;
+    //    boolean x=true;
+    //    for(int i=0;i<nums.length;i++){
+    //     if(avg+1==nums[i]){
+    //         avg+=1;
+    //     }
+    //    }
+    //    return avg+1;
+    int n=nums.length;
+  
+    HashSet<Integer>h=new HashSet<>();
+    for(int i=0;i<n;i++){
+        h.add(nums[i]);
+    }
+    // if(avg<0)avg=0;
+    // else avg/=nums.length;
+    // int max=avg;
+    //  for(int i=0;i<n;i++){
+    //     if(avg<nums[i]){
+    //         max=Math.max(avg,max);
+    //     }
+    // }
+    // return max;
+    for(int i=avg;i<=100;i++){
+         if(avg<i && !(h.contains(i))){
+            return i;
+         }
+         if(i==100)return i+1;
+    }
+    return avg+1;
     }
 }
