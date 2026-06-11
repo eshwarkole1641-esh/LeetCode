2d0ad788c6f3d389018bbbb8ec3cfb52241e0bfc
eshwarkole1641-esh <eshwarkole1641@gmail.com>
2026-06-11 21:10:04 +0530

    Time: 2 ms (67.61%), Space: 44.5 MB (37.5%) - LeetHub

--- 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.java
+++ 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.java
@@ -1,31 +1,48 @@
 class Solution {
 
-    public static boolean prime(int value,int i){
-        int count=0;
-            while(i<=value){
-                if(value%i==0){
-                    count++;
-                }
-            i++;
-        } 
-         if(count==2){
-                return true;
-            }   
-        return false;
+    public static boolean prime(int n){
+        // int count=0;
+        //     while(i<=value){
+        //         if(value%i==0){
+        //             count++;
+        //         }
+        //     i++;
+        // } 
+        //  if(count==2){
+        //         return true;
+        //     }   
+        // return false;
+         if(n < 2)
+            return false;
+
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+
+        return true;
     }
     public boolean checkPrimeFrequency(int[] nums) {
         HashMap <Integer,Integer>hm=new HashMap<>();
         for(int x:nums){
             hm.put(x,hm.getOrDefault(x,0)+1);
         }
-        for(Map.Entry<Integer,Integer>entry:hm.entrySet()){
-            int value=entry.getValue();
-            int i=1;
-            boolean res=prime(value,i);
-            if(res){
+        // for(Map.Entry<Integer,Integer>entry:hm.entrySet()){
+        //     int value=entry.getValue();
+        //     int i=1;
+        //     boolean res=prime(value,i);
+        //     if(res){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        for(int freq : hm.values()){
+            if(prime(freq)){
                 return true;
             }
         }
+
         return false;
     }
 }
